bce7fcc83bde3c169dbc0a96825e5bfa1a3f9f96
HETEPEMOK <bigoak2016@gmail.com>
2026-02-17 01:09:24 +0700

    Added test examples for Permission record

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void testUser()
+    public static void userTest()
     {
         System.out.println("===========User test===========");
         try {
@@ -8,7 +8,7 @@ public class Main {
         }
         catch (Exception e)
         {
-            System.out.println("[FAIL] User 1 (invalid) | " + e.getMessage());
+            System.out.println("[ERROR] User 1 (invalid) | " + e.getMessage());
         }
         try {
             User user2 = User.create("Username", "", "email@gmail.com");
@@ -16,29 +16,56 @@ public class Main {
         }
         catch (Exception e)
         {
-            System.out.println("[FAIL] User 2 (invalid) | " + e.getMessage());
+            System.out.println("[ERROR] User 2 (invalid) | " + e.getMessage());
         }
         try {
-            User user3 = User.create("Username", "Fullname", "email");
+            User user3 = User.create("Username", "Full name", "email");
             System.out.println("[SUCCESS] User 3 (invalid) | " + user3.format());
         }
         catch (Exception e)
         {
-            System.out.println("[FAIL] User 3 (invalid) | " + e.getMessage());
+            System.out.println("[ERROR] User 3 (invalid) | " + e.getMessage());
         }
         try {
             User user4 = User.create("Username", "Full name", "email@gmail.com");
-            System.out.println("[SUCCESS] User 2 (valid) | " + user4.format());
+            System.out.println("[SUCCESS] User 4 (valid) | " + user4.format());
         }
         catch (Exception e)
         {
             System.out.println("[ERROR] User 4 (valid) | " + e.getMessage());
         }
     }
-
+    public static void permissionTest()
+    {
+        System.out.println("========Permission test========");
+        try
+        {
+            Permission per1 = new Permission("read", "USERS", "Allows reading users");
+            System.out.println("[SUCCESS] Permission 1 | " + per1.format());
+        } catch (Exception e) {
+            System.out.println("[ERROR] Permission 1 | " + e.getMessage());;
+        }
+        try
+        {
+            Permission per2 = new Permission("READ EVERYTHING", "users", "Description");
+            System.out.println("[SUCCESS] Permission 2 | " + per2.format());
+        } catch (Exception e)
+        {
+            System.out.println("[ERROR] Permission 2 | " + e.getMessage());
+        }
+        try
+        {
+            Permission p3 = new Permission("DELETE", "users", "Can delete users");
+            System.out.println("matches DEL/use [true]: " + p3.matches("DEL", "use"));
+            System.out.println("matches WR/use [false]: " + p3.matches("WR", "use"));
+        } catch (Exception e)
+        {
+            System.out.println("[ERROR] Permission 3 | " + e.getMessage());
+        }
+    }
     public static void main(String args[])
     {
-        testUser();
-
+        userTest();
+        permissionTest();
     }
 }
