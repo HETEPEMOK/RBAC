a48340851a7b737560cfb5aba6e27fc33783e6e4
HETEPEMOK <bigoak2016@gmail.com>
2026-02-24 01:21:42 +0700

    bugfix

--- src/main/java/UserFilters.java
+++ src/main/java/UserFilters.java
@@ -24,6 +24,6 @@ public class UserFilters {
 
     public static UserFilter byFullNameContains(String substring)
     {
-        return user -> user.fullname().toLowerCase().contains(substring.toLowerCase());
+        return user -> user.fullname().equalsIgnoreCase(substring);
     }
 }
